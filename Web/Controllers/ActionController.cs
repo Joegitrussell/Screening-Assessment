863e27da6c39e130b1e5f1adf8959360c90c27f2
Joe Russell <112443220+Joegitrussell@users.noreply.github.com>
2025-07-08 16:04:12 +0100

    Complete
    Completed Assignment

--- Web/Controllers/ActionController.cs
+++ Web/Controllers/ActionController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Screening_Assessment.Models;
+using System;
+using System.Numerics;
+using System.Reflection.Metadata;
 using Web.Models;
 
 namespace Screening_Assessment.Controllers
@@ -39,6 +42,14 @@ namespace Screening_Assessment.Controllers
         public static ViewableObjectModel GetReversedString()
         {
             return _viewModel;
+        }
+
+        public static string GetStatement()
+        {
+            var statement = "I found the assignment interesting and enjoyable, it's been a while since I have played with MVC architecture, it was fun to blow of the cobwebs." +
+                " I Spent around two hours of actual work on it, and then with time spent learning how to do stuff etc. Good tech test i would say.";
+
+            return statement;
         }
     }
 }
